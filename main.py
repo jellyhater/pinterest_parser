65bbc89bb2303a39b23900a7fea90366ca91e062
Ilyas <anikanovilyas@gmail.com>
2023-07-21 15:30:40 +0300

    added output arg

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ if __name__ == "__main__":
     argparser.add_argument('login', type=str, help='Pinterest account login')
     argparser.add_argument('password', type=str, help='Pinterest account password')
     argparser.add_argument('query', type=str, help='Query', default="Khokhloma")
+    argparser.add_argument('output', type=str, help='Name of output file', default="dataset")
     args = argparser.parse_args()
 
     # sleepers to avoid not found errors
     parser = PinterestParser(args.query)
     time.sleep(5)
     parser.authorize(login=args.login, password=args.password)
-    time.sleep(5)
+    time.sleep(10)
 
-    for i in range(3):
-        parser.scroll()
-        time.sleep(5)
-        data = parser.get_data()
-        parser.save_data(f"dataset_{i}", data)
+    data = parser.get_data()
+    parser.save_data(args.output, data)
+
+    # for i in range(1, 4):
+    #     parser.scroll()
+    #     time.sleep(5)
+    #     data = parser.get_data()
+    #     parser.save_data(args.output, data)
